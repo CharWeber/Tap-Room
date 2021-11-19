efa13692207fb995253f459c5b9712448fff3b32
Charlie Weber <charlestweber@gmail.com>
2021-11-19 15:40:55 -0800

    Updates

---
+++ precache-manifest.e6723c1a19e3628a70aa3c5534bcdb4d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17d84c24721e9a2616f23e53237a646f",
+    "url": "/Tap-Room/index.html"
+  },
+  {
+    "revision": "bbd3e78134777af4126e",
+    "url": "/Tap-Room/static/css/main.c205a827.chunk.css"
+  },
+  {
+    "revision": "c1571ccd6eacc2f3f1ec",
+    "url": "/Tap-Room/static/js/2.99fb60df.chunk.js"
+  },
+  {
+    "revision": "d9962b85509a5df882f7",
+    "url": "/Tap-Room/static/js/3.3270ef3c.chunk.js"
+  },
+  {
+    "revision": "bbd3e78134777af4126e",
+    "url": "/Tap-Room/static/js/main.119d854f.chunk.js"
+  },
+  {
+    "revision": "ad9e877064159e76d6c2",
+    "url": "/Tap-Room/static/js/runtime-main.903a0310.js"
+  },
+  {
+    "revision": "8be58a2a99e360ce111526512b6f24f4",
+    "url": "/Tap-Room/static/media/kegBG.8be58a2a.jpg"
+  },
+  {
+    "revision": "45668efed9805b596b7900fb0a968953",
+    "url": "/Tap-Room/static/media/mainBG.45668efe.jpg"
+  }
+]);
